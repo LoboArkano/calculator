d860d9348b97ccbad5f7b8761d5b980b1353a8f6
LoboArkano <pipiolo_mananes@hotmail.com>
2020-12-03 17:18:08 -0600

    Update App component Now App is a class component Add state and handleClick function Render Display and ButtonPanel components

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,35 @@ import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
 
-const App = () => (
-  <>
-    <Display />
-    <ButtonPanel />
-  </>
-);
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    this.handleClick.bind(this);
+  }
+
+  handleClick(name) {
+    const result = calculate(this.state, name);
+    this.setState(() => ({
+      total: result.total,
+      next: result.next,
+      operation: result.operation,
+    }));
+  }
+
+  render() {
+    const { total } = this.state;
+    return (
+      <>
+        <Display result={total || undefined} />
+        <ButtonPanel clickHandler={this.handleClick} />
+      </>
+    );
+  }
+}
 
 export default App;
