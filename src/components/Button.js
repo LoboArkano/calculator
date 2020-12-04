c2ec6b0f2e085ff11374e9da8c58a9c7e074869e
LoboArkano <pipiolo_mananes@hotmail.com>
2020-12-04 12:28:13 -0600

    Implement handleClick function

--- src/components/Button.js
+++ src/components/Button.js
@@ -7,14 +7,15 @@ const btnType = name => (name === '0' ? 'large-btn' : 'btn');
 
 const btnBg = name => (operators.includes(name) ? 'orange-bg' : 'lg-bg');
 
-const Button = ({ name }) => (
-  <button className={`${btnType(name)} ${btnBg(name)}`} type="button">
+const Button = ({ name, handleClick }) => (
+  <button className={`${btnType(name)} ${btnBg(name)}`} onClick={() => handleClick(name)} type="button">
     {name}
   </button>
 );
 
 Button.propTypes = {
   name: PropTypes.string.isRequired,
+  handleClick: PropTypes.func.isRequired,
 };
 
 export default Button;
