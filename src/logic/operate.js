a9935be89e3e88676438b90e427ee2517e69c4b8
LoboArkano <pipiolo_mananes@hotmail.com>
2020-12-06 19:28:03 -0600

    Update big number operations

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,30 +1,32 @@
 import Big from 'big.js';
 
 const operate = (numberOne, numberTwo, operation) => {
+  const number1 = Big(numberOne);
+  const number2 = Big(numberTwo);
   let result = null;
 
   if (numberOne === null || numberTwo === null) {
     result = 'Syntax ERROR';
   } else {
     switch (operation) {
-      case '*':
-        result = Big(numberOne) * Big(numberTwo);
+      case 'x':
+        result = number1.times(number2);
         break;
-      case '/':
+      case '÷':
         if (numberTwo === 0) {
           result = 'Math ERROR';
         } else {
-          result = Big(numberOne) / Big(numberTwo);
+          result = number1.div(number2);
         }
         break;
       case '+':
-        result = Big(numberOne) + Big(numberTwo);
+        result = number1.plus(number2);
         break;
       case '-':
-        result = Big(numberOne) - Big(numberTwo);
+        result = number1.minus(number2);
         break;
       case '%':
-        result = (Big(numberTwo) / 100) * Big(numberOne);
+        result = (number2.div(100)).times(number1);
         break;
       default:
         result = 0;
@@ -32,7 +34,7 @@ const operate = (numberOne, numberTwo, operation) => {
     }
   }
 
-  return result;
+  return String(result);
 };
 
 export default operate;
